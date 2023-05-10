5cebbb23842a720e6cee12192eaab2387f7eae91
jonmatthis <jonmatthis@gmail.com>
2023-05-10 07:36:10 -0400

    basic default agent working

--- src/langchain_stuff/agents/agent.py
+++ src/langchain_stuff/agents/agent.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from typing import Union
 
 import toml
-from langchain import LLMChain
+from langchain import OpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory, ConversationSummaryMemory, \
     ConversationSummaryBufferMemory
@@ -40,20 +41,29 @@ class Agent:
 
         self._llm = self._configure_llm(**self._configuration["llm"], verbose=True)
         self._chat_prompt = self._create_chat_prompt()
-        # self._memory = self._configure_memory()
+        self._memory = self._configure_memory()
+        self._agent = self._configure_agent()
+        self._tools = self._configure_tools()
 
-        self._llm_chain = LLMChain(llm=self._llm,
-                                   prompt=self._chat_prompt,
-                                   # memory=self._memory,
-                                   verbose=True)
+        # self._llm_chain = LLMChain(llm=self._llm,
+        #                            prompt=self._chat_prompt,
+        #                            memory=self._memory,
+        #                            verbose=True)
+        self._agent_chain = initialize_agent(tools=self._tools,
+                                             llm=self._llm,
+                                             agent=self._agent,
+                                             memory=self._memory,
+                                             verbose=True, )
 
     def intake_message(self, message: str):
         logger.info(f"Received message: {message}")
-        return self._llm_chain.run(human_input=message, **self._prompt_input_variables)
+        return self._agent_chain.run(input=message)
+        # return self._llm_chain.run(human_input=message, **self._prompt_input_variables)
 
     async def aintake_message(self, message: str):
         logger.info(f"Received message: {message}")
-        return await self._llm_chain.arun(human_input=message, **self._prompt_input_variables)
+        return await self._agent_chain.arun(input=message)
+        # return await self._llm_chain.arun(human_input=message, **self._prompt_input_variables)
 
     def _configure_llm(self,
                        type: str = "ChatOpenAI",
@@ -83,6 +93,7 @@ class Agent:
             template=prompt_config["system_message_template"],
             input_variables=list(self._prompt_input_variables.keys())
         )
+        self._system_prompt.format_messages(**self._prompt_input_variables)
 
         self._human_message_prompt = HumanMessagePromptTemplate.from_template(
             template=prompt_config["human_message_template"],
@@ -110,12 +121,31 @@ class Agent:
             memory = ConversationSummaryBufferMemory(llm=self._configure_llm(model="gpt-3.5-turbo", temperature=0),
                                                      memory_key=memory_config["memory_key"],
                                                      return_messages=memory_config["return_messages"],
-                                                     k=memory_config["k"],
                                                      )
         else:
             raise NotImplementedError(f"Memory type {memory_config['type']} not implemented... YET!")
 
         return memory
+
+    def _configure_agent(self):
+        agent_config = self._configuration["agent"]
+        logger.info(f"Configuring agent of type {agent_config}")
+
+        if agent_config["type"] == "CHAT_CONVERSATIONAL_REACT_DESCRIPTION":
+            return AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION
+
+    def _configure_tools(self):
+        math_llm = OpenAI(temperature=0.0)
+        try:
+            tools =load_tools(
+            ["human", "llm-math"],
+            llm=math_llm,
+            )
+        except Exception as e:
+            logger.error(f"Error loading tools: {e}")
+            raise e
+
+        return tools
 
 
 async def ademo_main():
@@ -128,6 +158,7 @@ async def ademo_main():
     print(await agent.aintake_message(human_message_2))
     print("\n--Done!")
 
+
 def demo_main():
     agent = Agent()
     human_message_1 = "Hello, I am a human. I am here to talk to you about the weather."
@@ -137,6 +168,7 @@ def demo_main():
     print(f"\n--Human says: {human_message_2}")
     print(agent.intake_message(human_message_2))
     print("\n--Done!")
+
 
 if __name__ == "__main__":
     # Set the event loop policy to WindowsSelectorEventLoopPolicy on Windows
